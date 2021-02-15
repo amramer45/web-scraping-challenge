db96b5f0938cbb2b5e271ca4b26a3fa3fd02c56e
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-02-15 14:57:54 -0500

    app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -10,3 +10,17 @@ app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 #Routes
+@app.route("/")
+def index():
+    mars_data = mongo.db.mars_data.find_one()
+    return render_template("index.html", mars_data=mars_data)
+
+@app.route("/scrape")
+def scrape():
+    mars_data = mongo.db.mars_data
+    mars_news = scrape_mars.scrape()
+    mars_data.update({}, mars_news, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
